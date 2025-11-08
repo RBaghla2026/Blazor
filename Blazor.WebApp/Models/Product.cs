3bb117fba7565d32bb0618a0aa4973b707b45abd
Rahul Baghla <rahul.baghla92@hotmail.com>
2025-11-08 11:03:08 +0530

    One Way Data Binding

---
+++ Blazor.WebApp/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazor.WebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
